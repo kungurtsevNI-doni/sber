c6bc3b4a5e587568a3f2e0352e511692dc821e4f
Nikita <kungurtsevni@mail.ru>
2017-10-09 11:58:39 +0300

    first commit

---
+++ test_for_sber/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace test_for_sber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.priceDataSet = new test_for_sber.priceDataSet();
+            this.priceDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.priceDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.priceDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // priceDataSet
+            // 
+            this.priceDataSet.DataSetName = "priceDataSet";
+            this.priceDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // priceDataSetBindingSource
+            // 
+            this.priceDataSetBindingSource.DataSource = this.priceDataSet;
+            this.priceDataSetBindingSource.Position = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 33);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(363, 28);
+            this.comboBox1.TabIndex = 12;
+            this.comboBox1.SelectionChangeCommitted += new System.EventHandler(this.comboBox1_SelectionChangeCommitted_1);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 83);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(754, 313);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 408);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.MinimumSize = new System.Drawing.Size(800, 400);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.priceDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.priceDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource priceDataSetBindingSource;
+        private priceDataSet priceDataSet;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ test_for_sber/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.Caching;
+
+namespace test_for_sber
+{
+    public partial class Form1 : Form
+    {
+        private Entities.priceEntities context = new Entities.priceEntities(); //Объект контекст.
+        private ObjectCache cache = MemoryCache.Default; //Объект кэша.
+        public Form1()
+        {   
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var groups = context.group.ToList(); //Лист авторов.
+            //В лист объектов заносим пустой, дабы изначально ни одна группа не была выбрана.
+            List<Entities.group> list = new List<Entities.group>
+            {
+                new Entities.group {title_group=""},
+            };
+            //Все объекты групп заносим в список.
+            foreach (var group in groups) {
+                list.Add(new Entities.group { id_group = group.id_group, title_group = group.title_group });
+            }
+            comboBox1.DataSource = list; //Устанавливаем источник данным для выпадающего списка.
+            comboBox1.DisplayMember = "title_group"; //Устанавливаем то, что будет видеть пользователь.
+            comboBox1.ValueMember = "id_group"; //Устанавливаем значение, которое потребуется для определения списка продуктов.
+        }
+
+        private void comboBox1_SelectionChangeCommitted_1(object sender, EventArgs e)
+        {
+            var group = comboBox1.SelectedValue; //Получаем значение (id_group) элемента списка, которое выбрал пользователь.
+            CacheItem ch = cache.GetCacheItem(group.ToString()); //Получаем объект из кэша по id.
+            //Если объект из кэша пустой, получаем продукты и заносим их в кэш.
+            if (ch == null)
+            {
+                //Продукты с соответствующим id группы.
+                var products = context.products.Select(c => new
+                                                {
+                                                    id_product = c.id_product,
+                                                    title_product = c.title_product,
+                                                    id_group = c.id_group
+                                                })
+                                                .Where(c => c.id_group == (int)group)
+                                                .ToList(); 
+                // Cоздаем новый объект CacheItemPolicy, указывающий, что срок действия записи кэша истекает через 1000 секунд.
+                CacheItemPolicy policy = new CacheItemPolicy();
+                policy.AbsoluteExpiration = DateTimeOffset.Now.AddSeconds(1000.0);
+                //Присваеваем объекту item кэша значение = продуктам, ключ = id_group
+                ch = new CacheItem(group.ToString(), products);
+                //Заносим объект ch в кэш.
+                cache.Set(ch, policy);
+                //Выводим продукты из кэша.
+                dataGridView1.DataSource = cache.GetCacheItem(group.ToString()).Value;
+            }//Если объект по данному ключу уже есть в кэше, то его и выводим в таблицу.
+            else {
+                //Выводим продукты из кэша.
+                dataGridView1.DataSource = cache.GetCacheItem(group.ToString()).Value;
+            }  
+        }
+    }
+}
